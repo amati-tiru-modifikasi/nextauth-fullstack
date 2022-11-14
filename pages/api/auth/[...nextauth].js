3260996c89c4a01e97a4613e5b9390f25474a200
mdestafadilah <desta.08b@gmail.com>
2022-11-14 22:04:53 +0700

    TOKEN JWT MIDELWERE https://next-auth.js.org/configuration/nextjs#custom-jwt-decode-method https://next-auth.js.org/configuration/options#jwt-helper

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import jwt from "jsonwebtoken"
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -10,6 +11,18 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  jwt: {
+    // The maximum age of the NextAuth.js issued JWT in seconds.
+    // Defaults to `session.maxAge`.
+    maxAge: 60 * 60 * 24 * 30,
+    // You can define your own encode/decode functions for signing and encryption
+    async encode({ secret, token }) {
+        return jwt.sign(token, secret)
+    },
+    async decode({ secret, token }) {
+        return jwt.verify(token, secret)
+    },
+  }
 }
 
 export default NextAuth(authOptions)
